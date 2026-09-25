8392c05aebb594c1228f5b931f3b4ea4f5a3d51a
Noman0kp <nomankp1107@gmail.com>
2026-09-25 07:03:25 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/pipeline/leakdetection/LeakDetectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pipeline.leakdetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeakDetectionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeakDetectionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pipeline/leakdetection/LeakDetectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipeline.leakdetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeakDetectionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
